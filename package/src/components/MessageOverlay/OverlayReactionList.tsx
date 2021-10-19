99db9b4ee8de29d791c91b80d3f72a4d0df9d48c
EricH <AvarioDeveloper@gmail.com>
2021-10-18 19:28:11 -0600

    Increase touchable area for reactions [sc-46297]

--- package/src/components/MessageOverlay/OverlayReactionList.tsx
+++ package/src/components/MessageOverlay/OverlayReactionList.tsx
@@ -236,6 +236,7 @@ export const ReactionButton = <
       onHandlerStateChange={onTap}
     >
       <Animated.View
+        hitSlop={{ top: 10, bottom: 10, left: 10, right: 10 }}
         style={[index !== numberOfReactions - 1 ? styles.notLastReaction : {}, reaction, iconStyle]}
       >
         <Icon height={reactionSize} pathFill={grey} width={reactionSize} />
